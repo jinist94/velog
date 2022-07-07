17e1e5ce56ed7c4494d2c12d068bbad7ae9b7e61
jinist94 <325days@naver.com>
2022-07-07 17:57:58 +0900

    feat: posts page 추가

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,17 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import type { AppProps } from "next/app";
 
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
