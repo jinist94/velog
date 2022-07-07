96abb9479c8009902849f643a716aea498cc727e
jinist94 <325days@naver.com>
2022-07-07 18:09:17 +0900

    feat: HTTP Header 추가

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,26 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import type { AppProps } from "next/app";
+import nookies from "nookies";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:1337/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // 서버사이드에서도 사용할 수 있도록 cookie 사용.
+  const { token } = nookies.get();
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: "http://localhost:1337/graphql",
+  // uri: "http://localhost:1337/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
