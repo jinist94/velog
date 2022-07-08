feb43f33820e4f40368766cf550f520ba1656bd7
jinist94 <325days@naver.com>
2022-07-08 18:38:46 +0900

    feat: 로그인한 유저 정보 props 전달

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,16 +1,17 @@
-import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, createHttpLink, gql, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import type { AppContext, AppProps } from "next/app";
 import nookies from "nookies";
 import Header from "../components/Header";
+import { User } from "../interface";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:1337/graphql",
 });
 
-const authLink = setContext((_, { headers }) => {
+const authLink = setContext((_, { nextContext, headers }) => {
   // 서버사이드에서도 사용할 수 있도록 cookie 사용.
-  const { token } = nookies.get();
+  const { token } = nookies.get(nextContext);
   return {
     headers: {
       ...headers,
@@ -36,7 +37,26 @@ function MyApp({ Component, pageProps }: AppProps) {
 
 MyApp.getInitialProps = async (appCtx: AppContext) => {
   const { token } = nookies.get(appCtx.ctx);
-  return { pageProps: { token } };
+
+  /* 로그인한 유저의 정보 내려주기 */
+
+  let me: User | null = null;
+  if (token) {
+    const QUERY = gql`
+      query Me {
+        me {
+          id
+          username
+          email
+        }
+      }
+    `;
+
+    const { data } = await client.query<{ me: User }>({ query: QUERY, context: { nextContext: appCtx.ctx } });
+    me = data.me;
+    console.log(me, "me");
+  }
+  return { pageProps: { token, me } };
 };
 
 export default MyApp;
